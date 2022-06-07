1599da1d39902042e523ff200dbf0000bbea42c9
Tony Angello <taaffaat_12@hotmail.com>
2022-06-07 10:44:25 -0500

    back to entities as indexs

--- src/main.cpp
+++ src/main.cpp
@@ -19,19 +19,19 @@ struct PhysicsComponent_t
 };
 
 constexpr auto rendereable_printer [[maybe_unused]] = [](const auto& ren, const auto& pos, auto ent) {
-    std::cout << "[Found entity ID    ]: " << ent.GetIndex() << std::endl;
+    std::cout << "[Found entity ID    ]: " << ent.GetID() << std::endl;
     std::cout << "[RenderComponent    ]: " << ren.c << std::endl;
     std::cout << "[PositionComponent_t]: " << "X: " << pos.x << " Y: " << pos.y << std::endl << std::endl;
 };
 
 constexpr auto movable_printer [[maybe_unused]] = [](auto& phy, auto& pos, auto ent) {
-    std::cout << "[Found entity ID    ]: " << ent.GetIndex() << std::endl;
+    std::cout << "[Found entity ID    ]: " << ent.GetID() << std::endl;
     std::cout << "[PhysicsComponent_t ]: " << "VX: " << phy.vx  << " VY: " << phy.vy << std::endl;
     std::cout << "[PositionComponent_t]: " << "X: "  << pos.x   << " Y: "  << pos.y  << std::endl << std::endl;
 };
 
 constexpr auto basic_character_printer [[maybe_unused]] = [](auto& ren, auto& pos, auto& phy, auto ent) {
-    std::cout << "[Found entity ID    ]: " << ent.GetIndex() << std::endl;
+    std::cout << "[Found entity ID    ]: " << ent.GetID() << std::endl;
     std::cout << "[RenderComponent    ]: " << ren.c << std::endl;
     std::cout << "[PhysicsComponent_t ]: " << "VX: " << phy.vx  << " VY: " << phy.vy << std::endl;
     std::cout << "[PositionComponent_t]: " << "X: "  << pos.x   << " Y: "  << pos.y  << std::endl << std::endl;
@@ -50,10 +50,10 @@ int main()
     Args::Arguments_t pos_args { Args::For_v<PositionComponent_t>, 1, 2 };
     Args::Arguments_t phy_args { Args::For_v<PhysicsComponent_t>, 3, 4 };
 
-    auto ent1 [[maybe_unused]] = ecs_man.CreateEntity<Renderable_t>(ren_args1);
-    auto ent2 [[maybe_unused]] = ecs_man.CreateEntity<Renderable_t>(ren_args2, pos_args);
-    auto ent3 [[maybe_unused]] = ecs_man.CreateEntity<Movable_t>(pos_args, phy_args);
-    auto ent4 [[maybe_unused]] = ecs_man.CreateEntity<BasicCharacter_t>(Args::Arguments_t{ Args::For_v<RenderComponent_t>, 'd' });
+    ecs_man.CreateEntity<Renderable_t>(ren_args1);
+    ecs_man.CreateEntity<Renderable_t>(ren_args2, pos_args);
+    ecs_man.CreateEntity<Movable_t>(pos_args, phy_args);
+    ecs_man.CreateEntity<BasicCharacter_t>(Args::Arguments_t{ Args::For_v<RenderComponent_t>, 'd' });
 
     std::cout << "Iterating over renderables..." << std::endl;
     ecs_man.ForEachEntity<Renderable_t>(rendereable_printer);
@@ -63,9 +63,6 @@ int main()
 
     std::cout << "Iterating over basic characters..." << std::endl;
     ecs_man.ForEachEntity<BasicCharacter_t>(basic_character_printer);
-
-    std::cout << "===Transforming ent4 aka basic_character to renderable===" << std::endl;
-    ecs_man.TransformTo<Renderable_t>(ent4);
 
     std::cout << "Iterating over renderables..." << std::endl;
     ecs_man.ForEachEntity<Renderable_t>(rendereable_printer);
