b95eb42cd3bf1e5d615c50a968cf9e81fb435dc8
Tony Angello <taaffaat_12@hotmail.com>
2023-05-12 14:51:05 -0500

    removed ComponentWrapper as not needed

--- src/component_manager.hpp
+++ src/component_manager.hpp
@@ -6,38 +6,27 @@
 namespace ECS
 {
 
-template<class Cmpt_t>
-struct ComponentWrapper_t
-{
-    using type = Cmpt_t;
-
-    Cmpt_t mSelf {  };
-
-    template<class... Args_t> constexpr
-    ComponentWrapper_t(Args_t&&... args) : mSelf { std::forward<Args_t>(args)... } {  }
-};
-
 template<class CmptList_t>
 struct ComponentManager_t;
 
-template<class T> using ComponentKey_t = typename ECSMap_t<ComponentWrapper_t<T>>::Key_t;
+template<class T> using ComponentKey_t = typename ECSMap_t<T>::Key_t;
 
 template<template <class...> class CmptList_t, class... Cmpts_t>
 struct ComponentManager_t<CmptList_t<Cmpts_t...>> final
-    : SoA_t<ECSMap_t, ComponentWrapper_t<Cmpts_t>...>, Uncopyable_t
+    : SoA_t<ECSMap_t, Cmpts_t...>, Uncopyable_t
 {
 public:
                       using Self_t         = ComponentManager_t;
-                      using Base_t         = SoA_t<ECSMap_t, ComponentWrapper_t<Cmpts_t>...>;
+                      using Base_t         = SoA_t<ECSMap_t, Cmpts_t...>;
 
     constexpr explicit ComponentManager_t() : Base_t{  } {  }
 
-    template<class ReqCmpt_t, class... Args_t> constexpr auto
-    Create(Args_t&&... args) -> auto
+    template<class ReqCmpt_t> constexpr auto
+    Create(ReqCmpt_t cmp) -> auto
     {
-        using Component_t = ComponentWrapper_t<ReqCmpt_t>;
-
-        return Base_t::template emplace_back<Component_t>(std::forward<Args_t>(args)...);
+        //return Base_t::template emplace_back<ReqCmpt_t>(std::forward<Args_t>(args)...);
+        return Base_t::template emplace_back<ReqCmpt_t>(cmp);
+        //return Base_t::template push_back<ReqCmpt_t>(cmp);
     }
 
     template<class K_t> constexpr auto
@@ -54,19 +43,19 @@ public:
     GetComponent(K_t cmp_key) const -> const auto&
     {
         using Component_t = typename K_t::value_type;
-        return Base_t::template operator[]<Component_t>(cmp_key).mSelf;
+        return Base_t::template operator[]<Component_t>(cmp_key);
     }
 
     template<class K_t> constexpr auto
     GetComponent(K_t cmp_key) -> auto& 
     {
         using Component_t = typename K_t::value_type;
-        return Base_t::template operator[]<Component_t>(cmp_key).mSelf;
+        return Base_t::template operator[]<Component_t>(cmp_key);
     }
 
     template<class Cmpt_t> constexpr auto
     size() const -> auto
-    { return Base_t::template size<ComponentWrapper_t<Cmpt_t>>(); }
+    { return Base_t::template size<Cmpt_t>(); }
 private:
     using Base_t::operator[];
     using Base_t::at;
